8cefc94b285fe45421c813f3e08eb6af0537e6a3
CarolineGao <curiousgao@gmail.com>
2019-03-15 12:07:01 +1100

    update assets3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,10 +10,3 @@ module ApplicationHelper
     end
   end
 end
-
-module ApplicationHelper
-  # this method will embed the code from the partial
-  def youtube_video(url)
-    render :partial => 'shared/video', :locals => { :url => url }
-  end
-end
